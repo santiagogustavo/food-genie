c0b32575a085749e38b780894d7482d1de74e5de
santiagogustavo <santiago.gustavo.mail@gmail.com>
2023-02-01 00:41:42 -0300

    fix(teller): remove broken condition for current question

--- src/components/Teller.vue
+++ src/components/Teller.vue
@@ -157,9 +157,6 @@ watch(
 watch(
   () => currentQuestion.value,
   async next => {
-    if (!next) {
-      return;
-    }
     questionAlternatives.value = await getCurrentQuestion(next, userStore.value.latestAnswer?.name);
     if (questionAlternatives?.value[0]?.name === questionAlternatives?.value[1]?.name) {
       handleAnswerQuestion(questionAlternatives.value[0]);
